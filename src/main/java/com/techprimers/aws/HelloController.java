173420c37cc429cc91a10688fb45dadee1f5895a
rd2crt <79690747+rd2crt@users.noreply.github.com>
2021-03-24 12:16:26 +0000

    Update HelloController.java

--- src/main/java/com/techprimers/aws/HelloController.java
+++ src/main/java/com/techprimers/aws/HelloController.java
@@ -10,6 +10,6 @@ public class HelloController {
 
     @GetMapping
     public String hello() {
-        return "Hello Students!";
+        return "Hello Everyone, hope you have a nice day!";
     }
 }
